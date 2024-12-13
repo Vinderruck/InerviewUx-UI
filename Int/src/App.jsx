ff9bc9d155a516305ac73a5f3ec2ada3e7623bb4
Vinderruck <majevincent0@gmail.com>
2024-12-13 07:16:38 +0300

    changes

--- Int/src/App.jsx
+++ Int/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
      <BrowserRouter>
      
      <Routes>
-      <Route path ="/"  element={<Login/>}/>
-      <Route path ="/home"  element ={<Home/>}/>
+      <Route path ="/Login"  element={<Login/>}/>
+      <Route path ="/"  element ={<Home/>}/>
      </Routes>
      </BrowserRouter>
    
